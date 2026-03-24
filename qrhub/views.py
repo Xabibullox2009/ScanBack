baf509c2f683eded3f96567d0ef060389f122945
Habibulloh <habibulloh@example.com>
2026-03-24 22:58:48 +0500

    update

--- qrhub/views.py
+++ qrhub/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render
 
 from .models import QRCode
 
 
 def home(request):
-    return redirect("admin:index")
+    return render(request, "qrhub/home.html")
 
 
 def public_qr(request, slug):
